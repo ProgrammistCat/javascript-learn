c6e99891448c4ae77f44e86393fd05150f8cd065
ProgrammistCat <sofiapogorelova94@gmail.com>
2021-05-16 15:33:56 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+
+
+// alert('Hello world')
+
+
+// Пример JS
+// let block = document.querySelector('.block')
+//
+// let btnRed = document.querySelector('.btn-red')
+// let btnYellow = document.querySelector('.btn-yellow')
+// let btnPink = document.querySelector('.btn-pink')
+//
+// let btnBig = document.querySelector('.btn-big')
+// let btnSmall = document.querySelector('.btn-small')
+//
+//
+//
+//
+// btnRed.addEventListener('click', () => {
+//     block.style.background = 'red'
+// })
+//
+// btnYellow.addEventListener('click', () => {
+//     block.style.background = 'yellow'
+// })
+//
+// btnPink.addEventListener('click', () => {
+//     block.style.background = 'pink'
+// })
+//
+//
+// btnBig.addEventListener('click', () => {
+//     block.style.width = '500px'
+//     block.style.height = '500px'
+// })
+//
+// btnSmall.addEventListener('click', () => {
+//     block.style.width = '50px'
+//     block.style.height = '50px'
+// })
+
+
+/*
+* Переменные
+*
+* */
+
+let a = 1      // number
+let b = "str"  // string
+let c = true   // boolean (true, false)
+
+/*
+* Типы данных
+* number
+* string
+* boolean
+* */
+
+alert(a)
